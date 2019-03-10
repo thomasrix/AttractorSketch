468aaa4a8b0deab99edcd84fe4b51c450f2b0908
Thomas Rix <trix@dr.dk>
2019-03-10 21:06:25 +0100

    Drawing the FlowField

--- attractor-2d.js
+++ attractor-2d.js
@@ -6,27 +6,71 @@ const settings = {
 };
 
 const sketch = () => {
-  
-  const getValue = ()=>{
+  let parameters = {
+    // Values for the attractor function
+    a : -0.7,
+    b : 0.7,
+    c : -0.5,
+    d : 0.5
+  }
+
+  const getValue = (x, y, props)=>{
+    // console.log('gv', settings)
     // clifford attractor
     // http://paulbourke.net/fractals/clifford/
     
     // scale down x and y
-    var scale = 0.005;
+    let {width, height} = props;
+    
+    let scale = 0.005;
     x = (x - width / 2) * scale;
     y = (y - height / 2)  * scale;
     
     // attactor gives new x, y for old one. 
-    var x1 = Math.sin(parameters.a * y) + parameters.c * Math.cos(parameters.a * x);
-    var y1 = Math.sin(parameters.b * x) + parameters.d * Math.cos(parameters.b * y);
+    let x1 = Math.sin(parameters.a * y) + parameters.c * Math.cos(parameters.a * x);
+    let y1 = Math.sin(parameters.b * x) + parameters.d * Math.cos(parameters.b * y);
     
     // find angle from old to new. that's the value.
     return Math.atan2(y1 - y, x1 - x);
     
   }
-  return ({ context, width, height }) => {
+
+  const drawTheFlow = (props)=>{
+    const {context, width, height} = props;
+    const res = 35;
+    
+    context.strokeStyle = 'black';
+    context.fillStyle = 'black';
+    context.lineWidth = 1;
+    // context.clearRect(0, 0, width, height);
+    for(var x = 0; x < width; x += res) {
+        for(var y = 0; y < height; y += res) {
+            var value = getValue(x, y, props);
+            context.save();
+            context.translate(x, y);
+
+            context.beginPath();
+            context.arc(0, 0, 1.5, 0, Math.PI * 2, false);
+            context.fill();
+    
+            context.rotate(value);
+            context.beginPath();
+            context.moveTo(0, 0);
+            context.lineTo(res * 0.75, 0);
+            context.stroke();
+            //context.circle(2, 2);
+            //context.fill();
+            context.restore();
+        }
+    }
+  }
+
+  return (props) => {
+    const { context, width, height } = props;
+    // console.log(props);
     context.fillStyle = 'lightgrey';
     context.fillRect(0, 0, width, height);
+    drawTheFlow(props);
   };
 };
 
