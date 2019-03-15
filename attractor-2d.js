3322be320a4f60389f9c8f3e1d2611455e35013c
Thomas Rix <trix@dr.dk>
2019-03-15 17:47:11 +0100

    Distribute start positions with a sprinkle of randomness

--- attractor-2d.js
+++ attractor-2d.js
@@ -1,30 +1,28 @@
 const canvasSketch = require('canvas-sketch');
 const {lerp} = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [ 2048, 2048 ],
   scaleToView: true,
-  // animate:false,
-  // loop:true,
-  // fps:30,
-  // playbackRate:'fixed',
-  // duration:4,
 };
 
 const sketch = () => {
-  let bgColor = '#2b3a42';
+  let bgColor = '#1e3440';
   let lineColor = '#f0f0df';
   let parameters = {
     // Values for the attractor function
-    a : 3,
-    b : -2.4,
+    a : 1.9,
+    b : -0.9,
     c : -1.7,
-    d : 0.5
+    d : -0.4
   }
   let counter = {
-    startRadius:1000,
+    startRadius:1020,
     numOfParticles:1500,
-    numOfFrames:200
+    numOfFrames:300,
+    fadeIn:120,
+    fadeOut:80
   }
   let points;
   
@@ -43,68 +41,67 @@ const sketch = () => {
       angle = i*slice;
       
       points.push({
-        x: center.x + Math.cos(angle) * radius,
-        y: center.y + Math.sin(angle) * radius, 
+        x: center.x + Math.cos(angle) * random.range(radius * 0.996, radius),
+        y: center.y + Math.sin(angle) * random.range(radius * 0.996, radius), 
         vx: 0,
         vy: 0
       })
     };
     // console.log(points);
   }
-  
+  const envelope = (frame)=>{
+    let fadeOut = counter.numOfFrames - counter.fadeOut;
+    let maxAlpha = alpha = 0.05;
+    if(frame < counter.fadeIn){
+      alpha = lerp(0, maxAlpha, frame/counter.fadeIn);
+    }
+    else if(frame > fadeOut){
+      alpha = lerp(maxAlpha, 0, (frame - fadeOut) / (counter.numOfFrames - fadeOut));
+    }
+    // console.log('alpha', frame, alpha);
+    return alpha;
+  }
   const drawPoints = (props, frame)=>{
     // console.log(points)
     let {context, width, height} = props;
     let norm = frame/counter.numOfFrames;
-    console.log('norm', norm);
+    // console.log('norm', norm);
     context.strokeStyle = lineColor;
     context.globalAlpha = 0.1;
     context.lineWidth = 3;
-    context.globalAlpha = lerp(0, 0.1, frame/counter.numOfFrames);
+    context.globalAlpha = envelope(frame);
     // context.fillStyle = '#151515';
 
     for(let i = 0; i < points.length; i++) {
-      // get each point and do what we did before with a single point
+      // get each point and accellerate according to the attractor function
       let p = points[i];
       let value = getValue(p.x, p.y, props);
       p.vx += Math.cos(value) * 0.3;
       p.vy += Math.sin(value) * 0.3;
       
-      // move to current position
       context.beginPath();
+      // move to current position
       context.moveTo(p.x, p.y);
       
-      // add velocity to position and line to new position
+      // add velocity to position and draw line to new position
       p.x += p.vx;
       p.y += p.vy;
       context.lineTo(p.x, p.y);
       context.stroke();
 
-      // context.beginPath();
-      // context.arc(p.x, p.y, 5, 0, Math.PI * 2, false);
-      // context.fill();
-
-      
-      // apply some friction so point doesn't speed up too much
+      // slow down the accelleration
       p.vx *= 0.99;
       p.vy *= 0.99;
       
-      // wrap around edges of screen
+      // wrap the overflow
       if(p.x > width) p.x = 0;
       if(p.y > height) p.y = 0;
       if(p.x < 0) p.x = width;
       if(p.y < 0) p.y = height;
     }
     
-    // call this function again in one frame tick
-    /*       counter.tic++;
-    if(counter.tic<counter.maxFrames){
-      requestAnimationFrame(render);
-    }
-    */  
   }
   const getValue = (x, y, props)=>{
-    // console.log('gv', settings)
     // Using the Clifford Attractor
     // http://paulbourke.net/fractals/clifford/
     
@@ -121,16 +118,15 @@ const sketch = () => {
     
     // find angle from old to new. that's the value.
     return Math.atan2(y1 - y, x1 - x);
-    
   }
   
   const drawTheFlow = (props)=>{
     const {context, width, height} = props;
     const res = 35;
     
-    context.strokeStyle = '#151515';
-    context.fillStyle = '#151515';
-    context.lineWidth = 1;
+    context.strokeStyle = lineColor;
+    context.fillStyle = lineColor;
+    context.lineWidth = 2;
     // context.clearRect(0, 0, width, height);
     for(var x = 0; x < width; x += res) {
       for(var y = 0; y < height; y += res) {
@@ -139,7 +135,7 @@ const sketch = () => {
         context.translate(x, y);
         
         context.beginPath();
-        context.arc(0, 0, 1.5, 0, Math.PI * 2, false);
+        context.arc(0, 0, 3, 0, Math.PI * 2, false);
         context.fill();
         
         context.rotate(value);
@@ -151,7 +147,6 @@ const sketch = () => {
       }
     }
   }
-  
   
   return (props) => {
     const { context, width, height, playhead } = props;
